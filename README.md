eda2da9fda9e7af5609eeec177d5b410e23dd149
florian dahlitz <f2dahlitz@freenet.de>
2017-04-19 19:45:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Description ##
+The classic game tic-tac-toe as a simple js-based browser game.
+
+Try it yourself without downloding right in your browser:
+<a href="htmlpreview.github.io?https://github.com/DahlitzFlorian/tic-tac-toe">click here</a>.
---
+++ src/script.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+    /*
+        handles program flow
+     */
+    var counter = 2;
+    var end = false;
+    $("#tbody").find("td").on('click', function(event) {
+        if(!end) {
+            if($(this).text() === "") {
+                if(counter % 2 === 0) {
+                    $(this).text("X");
+                }
+                else {
+                    $(this).text("O");
+                }
+
+                counter++;
+                checkGame();
+
+                if(counter === 11) {
+                    end = true;
+                    alert("Game over! Nobody wins.");
+                }
+            }
+        }
+    });
+
+    /*
+        check if someone won
+     */
+    var checkGame = function() {
+        if($("#square11").text() !== "") {
+
+            if($("#square12").text() === $("#square11").text()) {
+                if($("#square11").text() === $("#square13").text()) {
+                    end = true;
+                    $("#square11").addClass('red');
+                    $("#square12").addClass('red');
+                    $("#square13").addClass('red');
+                    alert($("#square11").text() + " wins!");
+                }
+            }
+            
+            if($("#square21").text() === $("#square11").text()) {
+                if($("#square11").text() === $("#square31").text()) {
+                    end = true;
+                    $("#square11").addClass('red');
+                    $("#square21").addClass('red');
+                    $("#square31").addClass('red');
+                    alert($("#square11").text() + " wins!");
+                }
+            }
+
+            if($("#square22").text() === $("#square11").text()) {
+                if($("#square11").text() === $("#square33").text()) {
+                    end = true;
+                    $("#square11").addClass('red');
+                    $("#square22").addClass('red');
+                    $("#square33").addClass('red');
+                    alert($("#square11").text() + " wins!");
+                }
+            }
+        }
+        
+        if($("#square21").text() !== "") {
+            if($("#square22").text() === $("#square21").text()) {
+                if($("#square21").text() === $("#square23").text()) {
+                    end = true;
+                    $("#square21").addClass('red');
+                    $("#square22").addClass('red');
+                    $("#square23").addClass('red');
+                    alert($("#square21").text() + " wins!");
+                }
+            }
+        }
+        
+        if($("#square31").text() !== "") {
+
+            if($("#square32").text() === $("#square31").text()) {
+                if($("#square31").text() === $("#square33").text()) {
+                    end = true;
+                    $("#square31").addClass('red');
+                    $("#square32").addClass('red');
+                    $("#square33").addClass('red');
+                    alert($("#square31").text() + " wins!");
+                }
+            }
+
+            if($("#square22").text() === $("#square13").text()) {
+                if($("#square31").text() === $("#square13").text()) {
+                    end = true;
+                    $("#square31").addClass('red');
+                    $("#square22").addClass('red');
+                    $("#square13").addClass('red');
+                    alert($("#square13").text() + " wins!");
+                }
+            }
+        }
+
+        if($("#square12").text() !== "") {
+            if($("#square22").text() === $("#square12").text()) {
+                if($("#square12").text() === $("#square32").text()) {
+                    end = true;
+                    $("#square12").addClass('red');
+                    $("#square22").addClass('red');
+                    $("#square32").addClass('red');
+                    alert($("#square12").text() + " wins!");
+                }
+            }
+        }
+
+        if($("#square13").text() !== "") {
+            if($("#square23").text() === $("#square13").text()) {
+                if($("#square33").text() === $("#square13").text()) {
+                    end = true;
+                    $("#square13").addClass('red');
+                    $("#square23").addClass('red');
+                    $("#square33").addClass('red');
+                    alert($("#square13").text() + " wins!");
+                }
+            }
+        }
+    };
+});
